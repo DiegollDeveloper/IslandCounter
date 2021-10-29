6df1eab0360d5455f6ceedf257d1a5e200d8144e
Diego Gonzalez <diego18_98@hotmail.com>
2021-10-28 19:43:02 -0500

    Visual settings and optimice change the value of a field

--- lib/island_counter.dart
+++ lib/island_counter.dart
@@ -26,6 +26,9 @@ class _IslandCounterState extends State<IslandCounter> {
   List<List<List<int>?>> oneMatrix = [];
   int islandsCounter = 0;
 
+  Function eq = const ListEquality().equals;
+
+  ScrollController firstRowScrollController = ScrollController();
   ScrollController rowsScrollController = ScrollController();
 
   @override
@@ -35,10 +38,6 @@ class _IslandCounterState extends State<IslandCounter> {
       screenWidth = MediaQuery.of(context).size.width;
       screenHeight = MediaQuery.of(context).size.height -
           MediaQuery.of(context).viewPadding.top;
-      // rowsScrollController.addListener(() {
-      //   print(rowsScrollController.offset);
-      //   rowsScrollController.jumpTo(rowsScrollController.position.pixels);
-      // });
       generateMatrix();
     }
     super.didChangeDependencies();
@@ -57,17 +56,23 @@ class _IslandCounterState extends State<IslandCounter> {
         body: SingleChildScrollView(
       child: Container(
         width: double.infinity,
-        padding: EdgeInsets.symmetric(
-            vertical: screenHeight * 0.1, horizontal: screenWidth * 0.1),
+        padding: EdgeInsets.only(
+          top: screenHeight * 0.1,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Islands counter",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: screenWidth * 0.045)),
+            Text(
+              "Islands counter",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: screenWidth * 0.045),
+            ),
             Container(
-                margin: EdgeInsets.only(top: screenHeight * 0.02),
+                margin: EdgeInsets.only(
+                    top: screenHeight * 0.02,
+                    left: screenWidth * 0.1,
+                    right: screenWidth * 0.1,
+                    bottom: screenHeight * 0.02),
                 padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
                 decoration: BoxDecoration(
                     color: Colors.white,
@@ -116,36 +121,28 @@ class _IslandCounterState extends State<IslandCounter> {
                     )
                   ],
                 )),
-            Container(
-              margin: EdgeInsets.only(top: screenHeight * 0.02),
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  primary: false,
-                  itemCount: generatedMatrix.length,
-                  itemBuilder: (context, rowIndex) {
-                    return Center(
-                      child: SizedBox(
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              primary: true,
+              child: Container(
+                height: screenHeight * 0.6,
+                width: screenWidth * 0.1 * generatedMatrix.length,
+                margin: EdgeInsets.only(top: screenHeight * 0.02),
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    primary: false,
+                    itemCount: generatedMatrix.length,
+                    itemBuilder: (context, rowIndex) {
+                      return SizedBox(
                         height: screenHeight * 0.05,
                         child: ListView.builder(
-                            controller: rowsScrollController,
                             scrollDirection: Axis.horizontal,
                             shrinkWrap: true,
                             itemCount: generatedMatrix.length,
                             itemBuilder: (columContext, columnIndex) {
                               return GestureDetector(
                                 onTap: () {
-                                  setState(() {
-                                    if (generatedMatrix[rowIndex]
-                                            [columnIndex] ==
-                                        1) {
-                                      generatedMatrix[rowIndex][columnIndex] =
-                                          0;
-                                    } else {
-                                      generatedMatrix[rowIndex][columnIndex] =
-                                          1;
-                                    }
-                                  });
-                                  calculteOneMatrix();
+                                  changeFieldValue(rowIndex, columnIndex);
                                 },
                                 child: Container(
                                   width: screenWidth * 0.1,
@@ -158,21 +155,20 @@ class _IslandCounterState extends State<IslandCounter> {
                                         : Colors.lightBlue[100],
                                   ),
                                   alignment: Alignment.center,
-                                  child: Text(columnIndex.toString()),
+                                  child: Text(generatedMatrix[rowIndex]
+                                          [columnIndex]
+                                      .toString()),
                                 ),
                               );
                             }),
-                      ),
-                    );
-                  }),
+                      );
+                    }),
+              ),
             ),
             (generatedMatrix.isNotEmpty)
                 ? Container(
                     margin: EdgeInsets.only(top: screenHeight * 0.03),
-                    // height: screenHeight * 0.1,
-                    // width: screenWidth * 0.3,
                     alignment: Alignment.center,
-                    // color: Colors.grey,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -224,9 +220,9 @@ class _IslandCounterState extends State<IslandCounter> {
     }
   }
 
+  // Generate Square Matrix
   void generateMatrix() {
     try {
-      // Generate Square Matrix
       if (_matrixLengthController.text.isNotEmpty) {
         generatedMatrixLength = int.parse(_matrixLengthController.text);
         generatedMatrix.clear();
@@ -280,17 +276,22 @@ class _IslandCounterState extends State<IslandCounter> {
     }
   }
 
+  // Get the number of islands
   void calculteIslandsNumber() {
     try {
       List<Map<String, dynamic>> currentListElements = [];
       List<Map<String, dynamic>> nextListElements = [];
       List<Map<String, dynamic>> auxCurrentListElements = [];
       Map<String, dynamic> auxList = {"list": [], "added": false};
-
+      Map<String, dynamic> newMapList = {"list": [], "added": true};
+      List newList = [];
+      List<int> indexToConcat = [];
+      bool concat;
+      int counter = 0;
       islandsCounter = 0;
-      // Ones matrix row
+
+      // Go across row Ones matrix
       for (int rowIndex = 0; rowIndex < oneMatrix.length; rowIndex++) {
-        // bool contains = false;
         if (rowIndex == 0 && oneMatrix[rowIndex].isNotEmpty) {
           // Get list of maps from current row
           for (List<int>? rowMap in oneMatrix[rowIndex]) {
@@ -302,14 +303,14 @@ class _IslandCounterState extends State<IslandCounter> {
           for (var rowList in oneMatrix[rowIndex + 1]) {
             nextListElements.add({"list": List.of(rowList!), "added": false});
           }
-          // Current one matrix row element
+          // Go across current one matrix row element
           for (Map<String, dynamic> elementsList in currentListElements) {
             bool contains = false;
-            // next current one matrix row
+            // Go across next one matrix row
             for (int nexRowIndex = 0;
                 nexRowIndex < nextListElements.length;
                 nexRowIndex++) {
-              // next row elements
+              // Go across next row elements
               for (int i = 0;
                   i < nextListElements[nexRowIndex]["list"].length;
                   i++) {
@@ -339,30 +340,86 @@ class _IslandCounterState extends State<IslandCounter> {
             }
           }
 
-          Function eq = const ListEquality().equals;
-          List<int> indexToDelete = [];
-          for (int i = 0; i < auxCurrentListElements.length; i++) {
-            for (int j = i + 1;
-                j != i && j < auxCurrentListElements.length;
-                j++) {
-              if (i < auxCurrentListElements.length) {
-                List list1 = auxCurrentListElements[i]["list"];
-                List list2 = auxCurrentListElements[j]["list"];
-                if (eq(list1, list2)) {
-                  if (!indexToDelete.contains(j)) {
-                    indexToDelete.add(j);
+          newMapList = {"list": [], "added": true};
+          newList = [];
+          indexToConcat = [];
+          counter = 0;
+
+          // Verify is a group of elements belong to the same island
+          do {
+            concat = false;
+            for (int i = 0; i < auxCurrentListElements.length; i++) {
+              for (int j = i + 1;
+                  j != i && j < auxCurrentListElements.length;
+                  j++) {
+                if (i < auxCurrentListElements.length - 1) {
+                  List list1 = auxCurrentListElements[i]["list"];
+                  List list2 = auxCurrentListElements[j]["list"];
+
+                  for (int element in list2) {
+                    if (list1.contains(element)) {
+                      indexToConcat.add(i);
+                      indexToConcat.add(j);
+                      concat = true;
+                      break;
+                    }
                   }
                 }
+                if (concat) {
+                  break;
+                }
+              }
+              if (concat) {
+                break;
               }
             }
-          }
 
-          indexToDelete.sort((a, b) => b.compareTo(a));
-          for (int index in indexToDelete) {
-            auxCurrentListElements.removeAt(index);
-            // islandsCounter--;
+            if (concat) {
+              newList = auxCurrentListElements[indexToConcat[0]]["list"] +
+                  auxCurrentListElements[indexToConcat[1]]["list"];
+              newMapList["list"] = List.of(newList.toSet().toList());
+              indexToConcat.sort((a, b) => b.compareTo(a));
+              auxCurrentListElements.removeAt(indexToConcat[0]);
+              auxCurrentListElements.removeAt(indexToConcat[1]);
+              auxCurrentListElements.add(Map.of(newMapList));
+              newList.clear();
+              newMapList = {"list": [], "added": true};
+              indexToConcat.clear();
+            }
+            counter++;
+          } while (concat && counter < 100);
+          if (counter == 100) {
+            print(counter);
           }
+//
+          // List<int> indexToDelete = [];
+          // for (int i = 0; i < auxCurrentListElements.length; i++) {
+          //   for (int j = i + 1;
+          //       j != i && j < auxCurrentListElements.length;
+          //       j++) {
+          //     if (i < auxCurrentListElements.length) {
+          //       List list1 = auxCurrentListElements[i]["list"];
+          //       List list2 = auxCurrentListElements[j]["list"];
+          //       // for(int element in list2){
+          //       //   if(list1.contains(element)){
 
+          //       //   }
+          //       // }
+          //       if (eq(list1, list2)) {
+          //         if (!indexToDelete.contains(j)) {
+          //           indexToDelete.add(j);
+          //         }
+          //       }
+          //     }
+          //   }
+          // }
+
+          // indexToDelete.sort((a, b) => b.compareTo(a));
+          // for (int index in indexToDelete) {
+          //   auxCurrentListElements.removeAt(index);
+          //   // islandsCounter--;
+          // }
+//
           currentListElements = List.of(auxCurrentListElements);
           auxCurrentListElements.clear();
           nextListElements.clear();
@@ -377,6 +434,78 @@ class _IslandCounterState extends State<IslandCounter> {
       print("ISLANDS: " + islandsCounter.toString());
     } catch (e) {
       print("ERROR calculateIslandsNumber: $e");
+    }
+  }
+
+  // Change tapped field value and verify his elements around
+  changeFieldValue(int rowIndex, int colIndex) {
+    try {
+      // bool aboveIsland = false;
+      // bool rightIsland = false;
+      // bool belowIsland = false;
+      // bool leftIsland = false;
+      int islandsTogeter = 0;
+
+      // Verify field above
+      if ((rowIndex - 1 >= 0) && generatedMatrix[rowIndex - 1][colIndex] == 1) {
+        // aboveIsland = true;
+        islandsTogeter++;
+      }
+      // Verify field at right
+      if ((colIndex + 1 < generatedMatrix.length) &&
+          generatedMatrix[rowIndex][colIndex + 1] == 1) {
+        // rightIsland = true;
+        islandsTogeter++;
+      }
+      // Verify field below
+      if ((rowIndex + 1 < generatedMatrix.length) &&
+          generatedMatrix[rowIndex + 1][colIndex] == 1) {
+        // belowIsland = true;
+        islandsTogeter++;
+      }
+      // Verify field at left
+      if ((colIndex - 1 >= 0) && generatedMatrix[rowIndex][colIndex - 1] == 1) {
+        // leftIsland = true;
+        islandsTogeter++;
+      }
+
+      if (generatedMatrix[rowIndex][colIndex] == 1) {
+        // If field change to 0
+        if (islandsTogeter == 0) {
+          setState(() {
+            islandsCounter--;
+            generatedMatrix[rowIndex][colIndex] = 0;
+          });
+        } else if (islandsTogeter == 1) {
+          setState(() {
+            generatedMatrix[rowIndex][colIndex] = 0;
+          });
+        } else {
+          setState(() {
+            generatedMatrix[rowIndex][colIndex] = 0;
+          });
+          calculteOneMatrix();
+        }
+      } else {
+        // If field change to 1
+        if (islandsTogeter == 0) {
+          setState(() {
+            islandsCounter++;
+            generatedMatrix[rowIndex][colIndex] = 1;
+          });
+        } else if (islandsTogeter == 1) {
+          setState(() {
+            generatedMatrix[rowIndex][colIndex] = 1;
+          });
+        } else {
+          setState(() {
+            generatedMatrix[rowIndex][colIndex] = 1;
+          });
+          calculteOneMatrix();
+        }
+      }
+    } catch (e) {
+      print("ERROR changeFieldValue: $e");
     }
   }
 }
